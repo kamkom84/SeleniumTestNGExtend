f42cbc1989034977cfa23db342508988129f2bc1
АДМИН <kbddimitrov@gmail.com>
2024-04-30 07:35:59 +0300

    delete some imports

--- src/test/java/Tests/SeleniumTest.java
+++ src/test/java/Tests/SeleniumTest.java
@@ -5,16 +5,12 @@ import Pages.ProductsPage;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.io.IOException;
 
 public class SeleniumTest {
 
